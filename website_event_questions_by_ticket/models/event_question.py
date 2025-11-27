d99be8223fb313dd26e0305b2db8b6a03a24863b
Maxime Franco <maxime.franco@acsone.eu>
2025-11-27 11:58:32 +0100

    [19.0][MIG] website_event_questions_by_ticket: Migration to 19.0

---
+++ website_event_questions_by_ticket/models/event_question.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from typing import Self
+
+from odoo.orm.types import ValuesType
+
+from odoo import fields, models
+
+class EventQuestion(models.Model):
+    _inherit = "event.question"
+
+    restricted_ticket_ids = fields.Many2many(
+        comodel_name="event.event.ticket",
+        string="Limited to tickets",
+        domain="[('event_id', '=', parent.id)]",
+        ondelete="restrict",
+    )
